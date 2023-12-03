a12f84e2e2d74a0e75f0531a1069438124af0319
ilya-4real <allohaepta@gmail.com>
2023-12-04 00:48:29 +0200

    fixed api urls

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
 WEATHER_KEY = os.environ.get("WEATHER_API_KEY")
 CURRENCIES_KEY = os.environ.get("CURRENCY_API_KEY")
 
-WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
-CURRENCIES_API_URL = "http://api.exchangeratesapi.io/v1/latest?access_key={key}&symbols=UZS,RUB"
-
-cities = {
-    "kaliningrad": (54.7, 20.5),
-}
+WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+CURRENCIES_API_URL = "http://api.exchangeratesapi.io/v1/latest"
